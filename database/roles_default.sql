011acf8609975cfa80fd80125b26ab00207602c0
invertime <invertime.off@gmail.com>
2025-10-31 01:03:39 +0100

    chore: add request to test with default roles

---
+++ database/roles_default.sql
@@ -0,0 +1,13 @@
+INSERT INTO roles (title, description, permissions)
+VALUES (
+    'ceo',
+    'CEO',
+    '???'
+);
+
+INSERT INTO roles (title, description, permissions)
+VALUES (
+    'project-manager',
+    'Project Manager',
+    '???'
+);
